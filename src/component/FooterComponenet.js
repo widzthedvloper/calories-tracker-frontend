8b9122a428e2e06513927558a3e3ca980d343645
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-20 15:44:01 -0400

    Create FooterComponent

---
+++ src/component/FooterComponenet.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function FooterComponenet() {
+  return (
+    <div className="footer-nav">
+      <div className="nav-item">
+        <p>Add Food</p>
+      </div>
+      <div className="nav-item">
+        <p>Trac It</p>
+      </div>
+      <div className="nav-item">
+        <p>Your Health</p>
+      </div>
+      <div className="nav-item">
+        <p>More</p>
+      </div>
+    </div>
+  );
+}
+
+export default FooterComponenet;
