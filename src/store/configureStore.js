b63a9c6543328f79aeb14f7b5dbbb42acb25c96c
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-20 13:46:10 -0400

    Create store configuration function

---
+++ src/store/configureStore.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from '../reducers/rootReducer';
+
+const configureStore = (preloadedState) => {
+  const middlewares = [thunk];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+  const storeEnhancers = [middlewareEnhancer];
+  const composedEnhancer = compose(...storeEnhancers);
+
+  const store = createStore(
+    rootReducer,
+    preloadedState,
+    composedEnhancer,
+  );
+
+  return store;
+};
+
+export default configureStore;
