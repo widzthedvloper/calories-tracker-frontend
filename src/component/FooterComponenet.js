d9ded41f75e5496e3718b40a9937692bf3777e9e
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-20 15:46:04 -0400

    Add class

--- src/component/FooterComponenet.js
+++ src/component/FooterComponenet.js
@@ -4,16 +4,16 @@ function FooterComponenet() {
   return (
     <div className="footer-nav">
       <div className="nav-item">
-        <p>Add Food</p>
+        <p className="footer-text">Add Food</p>
       </div>
       <div className="nav-item">
-        <p>Trac It</p>
+        <p className="footer-text">Trac It</p>
       </div>
       <div className="nav-item">
-        <p>Your Health</p>
+        <p className="footer-text">Your Health</p>
       </div>
       <div className="nav-item">
-        <p>More</p>
+        <p className="footer-text">More</p>
       </div>
     </div>
   );
