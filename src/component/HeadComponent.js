84c4cb3b0b326ad30691069b2ddf9c6c9ef1b0ad
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-20 15:34:04 -0400

    Create head component

---
+++ src/component/HeadComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HeadComponent() {
+  return (
+    <div className="head-nav">
+      <h1>Calories Tracker</h1>
+    </div>
+  );
+}
+
+export default HeadComponent;
