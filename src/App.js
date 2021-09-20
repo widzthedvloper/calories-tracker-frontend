362db7ac6d956fda537752bec27ecd8cfd4fc61f
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-20 15:46:57 -0400

    add nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { connect } from 'react-redux';
+import HeadComponent from './component/HeadComponent';
 
 const mapState = (state) => ({
   userOauth: state.user.userOauth,
@@ -9,6 +10,7 @@ const mapState = (state) => ({
 function App({ userOauth }) {
   return (
     <div className="App">
+      <HeadComponent />
       Data form Redux:
       {' '}
       {userOauth}
