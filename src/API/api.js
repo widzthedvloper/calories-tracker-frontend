a7a173c82802bf95be18953b732088be08d5230b
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-21 19:11:15 -0400

    Add functions to send user data for sign in and sign up

--- src/API/api.js
+++ src/API/api.js
@@ -14,4 +14,20 @@ const logUser = async (user) => {
   }
 };
 
-export default logUser;
+const createUser = async (user) => {
+  try {
+    const response = await fetch('https://fierce-escarpment-48376.herokuapp.com/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+    return response.json();
+  } catch (error) {
+    return error.message;
+  }
+};
+
+export { logUser, createUser };
