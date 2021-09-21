95d3fab3b11d2c7dcc74ab2399e2c52483786e74
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-21 19:13:55 -0400

    Create Route component

--- src/component/Routes.js
+++ src/component/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from '../App';
 import SignInComponent from './SignInComponent';
+import SignUpComponent from './SignUpComponent';
 
 const Routes = () => (
   <BrowserRouter>
@@ -9,6 +10,7 @@ const Routes = () => (
       <Switch>
         <Route exact path="/" component={SignInComponent} />
         <Route exact path="/App" component={App} />
+        <Route exact path="/new/user" component={SignUpComponent} />
       </Switch>
     </div>
   </BrowserRouter>
