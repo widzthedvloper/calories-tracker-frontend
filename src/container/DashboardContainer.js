abd04fdba2c3a7f74e41b78364f9588acda7413e
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-27 14:09:41 -0400

    Create Dashboard container

---
+++ src/container/DashboardContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardComponent from '../component/DashboardComponent';
+
+function DashboardContainer() {
+  return (
+    <>
+      <DashboardComponent />
+    </>
+  );
+}
+
+export default DashboardContainer;
