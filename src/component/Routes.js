c08c2bf2205a0b04b6e1a7c969ae3b474b7ecba6
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-28 14:46:03 -0400

    Add route to add a food

--- src/component/Routes.js
+++ src/component/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from '../App';
+import AddFoodComponent from './AddFoodComponent';
 import SignInComponent from './SignInComponent';
 import SignUpComponent from './SignUpComponent';
 
@@ -11,6 +12,7 @@ const Routes = () => (
         <Route exact path="/" component={SignInComponent} />
         <Route exact path="/App" component={App} />
         <Route exact path="/new/user" component={SignUpComponent} />
+        <Route exact path="/new/food" component={AddFoodComponent} />
       </Switch>
     </div>
   </BrowserRouter>
