1fdda230d4d81aec3a0c94ee89c4d4799836f633
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-10-12 12:32:57 -0400

    Add icons

--- src/component/FooterComponent.js
+++ src/component/FooterComponent.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../style/footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faPlus,
+  faChartLine,
+  faTasks,
+  faEllipsisH,
+} from '@fortawesome/free-solid-svg-icons';
 
 function FooterComponent() {
   return (
     <div className="footer-nav">
       <div className="nav-item">
-        <NavLink className="footer-item" activeClassName="is-active" to="/new/food">
+        <FontAwesomeIcon icon={faPlus} style={{ marginLeft: '50%', marginTop: '15%'}}/>
+        <NavLink className="footer-item" activeClassName="is-active" to="/new/food" style={{marginLeft: '25%'}}>
           Add Food
         </NavLink>
       </div>
       <div className="nav-item">
-        <NavLink className="footer-text" activeClassName="is-active" to="/App">Dash</NavLink>
+        <FontAwesomeIcon icon={faChartLine} style={{ marginLeft: '50%', marginTop: '15%'}}/>
+        <NavLink className="footer-text" activeClassName="is-active" to="/App" style={{marginLeft: '25%'}} >Dash</NavLink>
       </div>
       <div className="nav-item">
-        <NavLink className="footer-text" activeClassName="is-active" to="/App">Ingredient</NavLink>
+        <FontAwesomeIcon icon={faTasks} style={{ marginLeft: '50%', marginTop: '15%'}}/>
+        <NavLink className="footer-text" activeClassName="is-active" to="/App" style={{marginLeft: '-6%'}} >Ingredient</NavLink>
       </div>
       <div className="nav-item">
-        <NavLink className="footer-text" activeClassName="is-active" to="/App">More</NavLink>
+        <FontAwesomeIcon icon={faEllipsisH} style={{ marginLeft: '50%', marginTop: '15%'}}/>
+        <NavLink className="footer-text" activeClassName="is-active" to="/App" style={{marginLeft: '25%'}} >More</NavLink>
       </div>
     </div>
   );
