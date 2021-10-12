d231e68e8cd9dfc688a66eb2690523c6306a13d3
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-10-12 14:06:49 -0400

    Style navbar

--- src/component/AddFoodComponent.js
+++ src/component/AddFoodComponent.js
@@ -3,6 +3,14 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { useHistory, NavLink } from 'react-router-dom';
 import { createFood } from '../API/api';
+import '../style/footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faPlus,
+  faChartLine,
+  faTasks,
+  faEllipsisH,
+} from '@fortawesome/free-solid-svg-icons';
 
 const mapState = (state) => ({
   id: state.user.id,
@@ -32,21 +40,47 @@ function AddFoodComponent({ id }) {
         </form>
       </div>
       <div className="footer-nav">
-        <div className="nav-item">
-          <NavLink className="footer-item" activeClassName="is-active" to="/new/food">
+      <div className="nav-item">
+        <NavLink className="footer-item" activeClassName="is-active" to="/new/food" style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
+          <span style={{display: 'flex', justifyContent: 'center'}}>
+            <FontAwesomeIcon icon={faPlus} style={{ display: 'block'}} />
+          </span>
+          <span style={{display: 'flex', justifyContent: 'center'}}>
             Add Food
-          </NavLink>
-        </div>
-        <div className="nav-item">
-          <NavLink className="footer-text" activeClassName="is-active" to="/App">Dash</NavLink>
-        </div>
-        <div className="nav-item">
-          <NavLink className="footer-text" activeClassName="is-active" to="/App">Ingredient</NavLink>
-        </div>
-        <div className="nav-item">
-          <NavLink className="footer-text" activeClassName="is-active" to="/App">More</NavLink>
-        </div>
+          </span>
+        </NavLink>
       </div>
+      <div className="nav-item">
+        <NavLink className="footer-text" activeClassName="is-active" to="/App" style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}} >
+          <span style={{display: 'flex', justifyContent: 'center'}}>
+          <FontAwesomeIcon icon={faChartLine} style={{ display: 'block'}} />
+          </span>
+          <span style={{display: 'flex', justifyContent: 'center'}}>
+            Dash
+          </span>
+        </NavLink>
+      </div>
+      <div className="nav-item">
+        <NavLink className="footer-text" activeClassName="is-active" to="/App" style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}} >
+          <span style={{display: 'flex', justifyContent: 'center'}}>
+            <FontAwesomeIcon icon={faTasks} style={{ display: 'block'}} />
+          </span>
+          <span style={{display: 'flex', justifyContent: 'center'}}>
+            Ingredient
+          </span>
+        </NavLink>
+      </div>
+      <div className="nav-item">
+        <NavLink className="footer-text" activeClassName="is-active" to="/App" style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}} >
+          <span style={{display: 'flex', justifyContent: 'center'}}>
+            <FontAwesomeIcon icon={faEllipsisH} style={{ display: 'block'}} />
+          </span>
+          <span style={{display: 'flex', justifyContent: 'center'}}>
+            More
+          </span>
+        </NavLink>
+      </div>
+    </div>
     </>
   );
 }
